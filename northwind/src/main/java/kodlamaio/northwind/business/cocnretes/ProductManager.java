f4f2d17207919af61fd101fd86c683e8603ab2b9
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2021-05-22 23:53:49 +0300

    22.05.2021

--- northwind/src/main/java/kodlamaio/northwind/business/cocnretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/cocnretes/ProductManager.java
@@ -5,22 +5,33 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
+
 @Service
 public class ProductManager implements ProductService {
 	ProductDao productDao;
+
 	@Autowired
 	public ProductManager(ProductDao productDao) {
 		this.productDao = productDao;
 	}
 
 	@Override
-	public List<Product> getAll() {
-		return this.productDao.findAll();
+	public DataResult<List<Product>> getAll() {
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(), "Data Listelendi.");
+	}
+
+	@Override
+	public Result add(Product product) {
+		this.productDao.save(product);
+		return new SuccessResult("Ürün Eklendi");
+
 	}
 
 }
